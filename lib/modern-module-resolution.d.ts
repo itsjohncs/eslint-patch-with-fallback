a5125934f7fac559c14fdd28ad46f771d5a2f830
itsjohncs <johnsullivan.pem@gmail.com>
2022-09-06 00:24:07 -0700

    Make usable as a fork.

---
+++ lib/modern-module-resolution.d.ts
@@ -0,0 +1,13 @@
+declare const path: any;
+declare const fs: any;
+declare const isModuleResolutionError: (ex: unknown) => boolean;
+declare let eslintrcBundlePath: string | undefined;
+declare let configArrayFactoryPath: string | undefined;
+declare let moduleResolverPath: string | undefined;
+declare let eslintFolder: string | undefined;
+declare const eslintPackageJson: any;
+declare const eslintPackageObject: any;
+declare const eslintPackageVersion: any;
+declare const versionMatch: RegExpExecArray;
+declare const eslintMajorVersion: number;
+declare let ConfigArrayFactory: any;
